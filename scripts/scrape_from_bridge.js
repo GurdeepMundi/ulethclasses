a4d2dcbff42c74606481f490159bd0c01e1fe788
Gurdeep Singh <gurdeepmundi10@gmail.com>
2023-07-19 10:19:43 -0600

    Improvements in the scraping script to scrape all of the courses from bridge

--- scripts/scrape_from_bridge.js
+++ scripts/scrape_from_bridge.js
@@ -2,26 +2,41 @@ const doc = document.body
 
 // Query and extract the desired elements
 const elements = doc.querySelectorAll('td.dddefault');
-const items = [];
-elements.forEach((element) => {
-  const item = {
-    CRN: elements[0].nextElementSibling.textContent.trim(),
-    Subject: elements[0].nextElementSibling.nextElementSibling.textContent,
-    CRSE: elements[0].nextElementSibling.nextElementSibling.nextElementSibling.textContent,
-    Section: elements[0].nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.textContent,
-    Campus: elements[0].nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.textContent,
-    Credits: elements[0].nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.textContent,
-    Title: elements[0].nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.textContent,
-    Days: elements[0].nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.textContent,
-    Time: elements[0].nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.textContent,
-    Instructor: elements[0].nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.textContent,
-    Date: elements[0].nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.textContent,
-    Location: elements[0].nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.textContent,
-    Attribute: elements[0].nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.textContent,
-  };
-})
-  
-  
-
-// Output the extracted items
-console.log(item);
+const courses = [];
+let ptr = 0;
+while (ptr < elements.length){
+    course_fields = [];
+    // for loop to collect all the fields for a course
+    for (let i = 0; i < 15; i++) {
+        try{
+            course_fields.push(elements[ptr+i].textContent.trim());
+        } catch (err) {
+            console.log(err);
+            ptr += 14;
+            break; // go to the next course
+        }
+    }
+    // Create a course object
+    const course = {
+        Availability: (course_fields[0] == 'C')? 'Closed' : 'Open',
+        CRN: course_fields[1],
+        Subject: course_fields[2],
+        CRSE: course_fields[3],
+        Section: course_fields[4],
+        Campus: course_fields[5],
+        Credits: course_fields[6],
+        Title: course_fields[7],
+        Days: course_fields[8],
+        Time: course_fields[9],
+        Instructor: course_fields[10],
+        Date: course_fields[11],
+        Location: course_fields[12],
+        Attribute: course_fields[13]
+    };
+    // Push the course object to the courses array
+    courses.push(course);
+    // Increment the pointer to the next course
+    ptr += 14;
+}
+// print the courses array
+console.log(courses);
